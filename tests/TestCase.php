368f71be1979d9f36a9cf4031bde2b52b4f9878a
yalin <yalincobanoglu@yandex.com>
2018-07-14 13:15:03 +0300

    db refresh , migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,12 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RefreshDatabase;
+    use DatabaseMigrations;
     use CreatesApplication;
 }
