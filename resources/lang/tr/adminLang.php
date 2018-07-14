f12dec6eb13831e720d0c378448c973a62dc2a81
yalin <yalincobanoglu@yandex.com>
2018-07-14 15:53:00 +0300

    lang

--- resources/lang/tr/adminLang.php
+++ resources/lang/tr/adminLang.php
@@ -2,5 +2,23 @@
 
 return [
     'admin-panel' => 'YÖNETİM PANELİ',
-    'logout' => 'Çıkış Yap'
+    'logout' => 'Çıkış Yap',
+    'new' => 'Ekle',
+    'show' => 'Göster',
+    'edit' => 'Düzenle',
+    'delete' => 'Sil',
+    'apply-filter' => 'Filtrele',
+    'search' => 'Ara',
+    'name' => 'İsim',
+    'add' => 'Ekleme',
+    'update' => 'Güncelleme',
+    'actions' => 'İşlemler',
+    'customers' => 'Müşteriler',
+    'create-customer' => 'Müşteri Girişi',
+    'phone' => 'Telefon',
+    'email' => 'E-posta',
+    'reset' => 'Temizle',
+    'save' => 'Kaydet',
+    'cancel' => 'İptal',
+
 ];
