ddaa724f81afd3f6cb67857c6361675a821ea38e
yalin <yalincobanoglu@yandex.com>
2018-07-14 12:20:34 +0300

    handler exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if (App::environment('testing')) {
+            throw $exception;
+        }
         return parent::render($request, $exception);
     }
 }
