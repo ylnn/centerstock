5927a3bd10f81f2a0e0e908c5292927743dce78f
yalin <yalincobanoglu@yandex.com>
2018-07-16 11:13:12 +0300

    customer routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 Route::get('/', 'Admin\DashboardController@index')->name('dashboard');
+
+// Customer
+Route::get('/customer', 'Admin\CustomerController@index')->name('admin.customer.index');
+Route::get('/customer/create', 'Admin\CustomerController@create')->name('admin.customer.create');
+Route::post('/customer/store', 'Admin\CustomerController@store')->name('admin.customer.store');
+Route::get('/customer/show/{customer}', 'Admin\CustomerController@show')->name('admin.customer.show');
+Route::get('/customer/edit/{customer}', 'Admin\CustomerController@edit')->name('admin.customer.edit');
+Route::post('/customer/update/{customer}', 'Admin\CustomerController@update')->name('admin.customer.update');
+Route::post('/customer/delete/{customer}', 'Admin\CustomerController@delete')->name('admin.customer.delete');
