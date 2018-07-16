33dc0290410d0b175a653770bfb036166c090e8b
yalin <yalincobanoglu@yandex.com>
2018-07-16 12:17:54 +0300

    area links

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,13 @@ Route::get('/customer/show/{customer}', 'Admin\CustomerController@show')->name('
 Route::get('/customer/edit/{customer}', 'Admin\CustomerController@edit')->name('admin.customer.edit');
 Route::post('/customer/update/{customer}', 'Admin\CustomerController@update')->name('admin.customer.update');
 Route::post('/customer/delete/{customer}', 'Admin\CustomerController@delete')->name('admin.customer.delete');
+
+
+// Area
+Route::get('/area', 'Admin\AreaController@index')->name('admin.area.index');
+Route::get('/area/create', 'Admin\AreaController@create')->name('admin.area.create');
+Route::post('/area/store', 'Admin\AreaController@store')->name('admin.area.store');
+Route::get('/area/show/{area}', 'Admin\AreaController@show')->name('admin.area.show');
+Route::get('/area/edit/{area}', 'Admin\AreaController@edit')->name('admin.area.edit');
+Route::post('/area/update/{area}', 'Admin\AreaController@update')->name('admin.area.update');
+Route::post('/area/delete/{area}', 'Admin\AreaController@delete')->name('admin.area.delete');
