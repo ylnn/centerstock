07cc74c1a227e5fe7ca7d5afb2a8c8fffce4cc9a
yalin <yalincobanoglu@yandex.com>
2018-07-16 15:39:57 +0300

    customer area relation test

--- tests/Feature/Admin/CustomerAreaTest.php
+++ tests/Feature/Admin/CustomerAreaTest.php
@@ -21,4 +21,44 @@ class CustomerAreaTest extends TestCase
         $this->assertTrue($customerArea = Customer::where('area_id', $area->id)->exists());
 
     }
+
+    public function testCustomerAreaRelationStore()
+    {
+        $area = factory(Area::class)->create();
+
+        $customer = factory(Customer::class)->make(['name' => 'test_relation_customer']);
+
+        $response = $this->post(route('admin.customer.store'), [
+            'name' => $customer->name, 
+            'area_id'=> $area->id
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('customers', [
+            'name' => 'test_relation_customer',
+            'area_id' => $area->id
+        ]);
+    }
+
+    public function testCustomerAreaRelationUpdate()
+    {
+        $area = factory(Area::class)->create();
+
+        $customer = factory(Customer::class)->create(['area_id' => $area->id, 'name' => 'CustomerAreaUpdateCustomerTestName']);
+
+        $secondArea = factory(Area::class)->create();
+
+        $response = $this->post(route('admin.customer.update', ['customer'=>$customer->id]), [
+            'name' => $customer->name,
+            'area_id'=> $secondArea->id
+        ]);
+
+        $response->assertStatus(302);
+
+        $this->assertDatabaseHas('customers', [
+            'id' => $customer->id,
+            'area_id' => $secondArea->id
+        ]);
+    }
 }
