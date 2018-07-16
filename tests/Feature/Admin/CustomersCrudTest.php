41f6d742116713beb2944ea96527aa9446c44766
yalin <yalincobanoglu@yandex.com>
2018-07-16 11:12:19 +0300

    customer crud test

--- tests/Feature/Admin/CustomersCrudTest.php
+++ tests/Feature/Admin/CustomersCrudTest.php
@@ -19,7 +19,7 @@ class CustomersCrudTest extends TestCase
         $response = $this->get(route('admin.customer.index'));
         $response->assertStatus(200);
         for ($i=0; $i < 3; $i++) {
-            $response->assertSee($customers[$i]->name);
+            $response->assertSee(e($customers[$i]->name));
         }
     }
 
@@ -34,5 +34,38 @@ class CustomersCrudTest extends TestCase
         $customer = factory(Customer::class)->make();
         $response = $this->post(route('admin.customer.store', ['name' => $customer->name]));
         $this->assertTrue(Customer::where('name', $customer->name)->exists());
+    }
+
+    public function testShowCustomerPage()
+    {
+        $customer = factory(Customer::class)->create();
+        $response = $this->get(route('admin.customer.show',['customer' => $customer->id]));
+        $response->assertStatus(200);
+        $response->assertSee(e($customer->name));
+    }
+
+    public function testEditCustomerPage()
+    {
+        $customer = factory(Customer::class)->create();
+        $response = $this->get(route('admin.customer.edit',['customer' => $customer->id]));
+        $response->assertStatus(200);
+        $response->assertSee(e($customer->name));
+    }
+
+    public function testUpdateCustomer()
+    {
+        $customer = factory(Customer::class)->create();
+        $this->post(route('admin.customer.update', ['customer' => $customer->id]), [
+            'name' => $customer->name,
+            'phone' => '123456'
+        ]);
+        $this->assertTrue(Customer::where('id', $customer->id)->where('phone', '123456')->exists());
+    }
+
+    public function testCustomerDelete()
+    {
+        $customer = factory(Customer::class)->create();
+        $this->post(route('admin.customer.delete', ['customer' => $customer->id]));
+        $this->assertFalse(Customer::where('id', $customer->id)->exists());
     }
 }
