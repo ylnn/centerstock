3a2901733cb84455fe18c9d009dcfe3faae4e3ca
yalin <yalincobanoglu@yandex.com>
2018-07-16 12:30:10 +0300

    fillable

--- app/Area.php
+++ app/Area.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Area extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = ['name', 'desc'];
     
     protected $dates = ['deleted_at'];
 }
