7aeaa41c45384b5c909120e8a6d08d2579a60f92
yalin <yalincobanoglu@yandex.com>
2018-07-20 16:56:35 +0300

    stock relation

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'base_price'];
+
+    public function stocks()
+    {
+        return $this->hasMany('App\Stock');
+    }
 }
