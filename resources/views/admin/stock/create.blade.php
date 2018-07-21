cc5675a1208aef79723881b44167246f6ba52c11
yalin <yalincobanoglu@yandex.com>
2018-07-21 11:03:17 +0300

    header update

--- resources/views/admin/stock/create.blade.php
+++ resources/views/admin/stock/create.blade.php
@@ -1,10 +1,15 @@
 @extends('layout.admin')
 
 @section('content')
-    <h2>@lang('adminLang.create-stock')</h2>
-    
-    <div class="row">
-        <div class="col-6">
+
+<div class="row">
+    <div class="col-6">
+
+        <div class="mb-4">
+            <h2>@lang('adminLang.adminLang.stock') @lang('adminLang.create')</h2>
+            <h4 class="alert alert-info">{{$product->name}}</h4>
+        </div>
+
         <form method="POST" action="{{ route($baseRoute . '.store', ['product' => $product->id]) }}">
                 {{csrf_field()}}
                 <div class="form-group">
@@ -19,11 +24,26 @@
                     <label for="purchase_price">@lang('adminLang.purchase_price')</label>
                     <input type="text" class="form-control" id="purchase_price" name="purchase_price">
                 </div>
+                <div class="form-group">
+                    <label for="sale_price">@lang('adminLang.sale_price')</label>
+                    <input type="text" class="form-control" id="sale_price" name="sale_price">
+                </div>
+                <div class="form-group">
+                    <label for="user_id">USER (DEĞİŞECEK)</label>
+                    <input type="text" class="form-control" id="user_id" name="user_id" value="1">
+                </div>
                 <button type="submit" class="btn btn-primary">@lang('adminLang.save')</button>
                 <a href="{{ url()->previous() }}" class="btn btn-secondary">@lang('adminLang.cancel')</a>
                 <input type="hidden" name="previous" value="{{ url()->previous() }}">
+                <input type="hidden" name="product_id" value="{{$product->id}}">
             </form>
 
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    
+
+    
+@endpush
--- resources/views/admin/stock/edit.blade.php
+++ resources/views/admin/stock/edit.blade.php
@@ -1,23 +1,33 @@
 @extends('layout.admin')
 
 @section('content')
-    <h2>@lang('adminLang.stock') @lang('adminLang.edit')</h2>
     <div class="row">
         <div class="col-6">
+            <div class="mb-4">
+                <h2>@lang('adminLang.stock') @lang('adminLang.edit')</h2>
+                <h4 class="alert alert-info">{{$product->name}}</h4>
+            </div>
         <form method="POST" action="{{ route($baseRoute . '.update', ['product' => $product->id, 'stock' => $record->id]) }}">
                 {{csrf_field()}}
-                
-                <div class="form-group">
-                    <label for="product-name">@lang('adminLang.product-name')</label>
-                    <p>{{$record->product->name}}</p>
-                </div>
                 <div class="form-group">
                     <label for="serial">@lang('adminLang.serial')</label>
                     <input type="text" class="form-control" id="serial" name="serial" value="{{$record->serial}}" autofocus required>
                 </div>
                 <div class="form-group">
                     <label for="quantity">@lang('adminLang.quantity')</label>
-                    <input type="text" class="form-control" id="quantity" name="quantity" value="{{$record->quantity}}" autofocus required>
+                    <input type="text" class="form-control" id="quantity" name="quantity" value="{{$record->quantity}}" >
+                </div>
+                <div class="form-group">
+                    <label for="purchase_price">@lang('adminLang.purchase_price')</label>
+                    <input type="text" class="form-control" id="purchase_price" name="purchase_price" value="{{$record->purchase_price}}">
+                </div>
+                <div class="form-group">
+                    <label for="sale_price">@lang('adminLang.sale_price')</label>
+                    <input type="text" class="form-control" id="sale_price" name="sale_price" value="{{$record->sale_price}}">
+                </div>
+                <div class="form-group">
+                    <label for="user_id">USER (DEĞİŞECEK)</label>
+                    <input type="text" class="form-control" id="user_id" name="user_id" value="1" value="{{$record->user_id}}">
                 </div>
 
                 <button type="submit" class="btn btn-primary">@lang('adminLang.save')</button>
@@ -29,3 +39,21 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script src="{{asset('js/jquery.priceformat.min.js')}}"></script>
+
+    <script>
+        $('#purchase_price').priceFormat({
+            prefix: 'TL ',
+            centsSeparator: ',',
+            thousandsSeparator: '.'
+        });
+
+        $('#sale_price').priceFormat({
+            prefix: 'TL ',
+            centsSeparator: ',',
+            thousandsSeparator: '.',
+        });
+    </script>
+@endpush
