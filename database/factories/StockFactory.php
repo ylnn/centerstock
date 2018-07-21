584b723af8ad4141fa7155daab185ecb19bbec37
yalin <yalincobanoglu@yandex.com>
2018-07-21 12:56:50 +0300

    stock factory decimal

--- database/factories/StockFactory.php
+++ database/factories/StockFactory.php
@@ -7,26 +7,40 @@ use Faker\Generator as Faker;
 use Carbon\Carbon;
 
 $factory->define(Stock::class, function (Faker $faker) {
-    // $purchase_price = $faker->numberBetween(300, 2000);
-    $purchasePrice = round($faker->randomFloat($nbMaxDecimals = 4, $min = 1, $max = 45), 2); // 48.8932
-    // $diff_price = $faker->numberBetween(10, 100);
-    $salePrice = round($purchasePrice * 1.2, 2);
-    $quantityArray = array(5,0,55,110,260,340,580,727,1450);
-    $qKey = array_rand($quantityArray);
     if (!Product::first()) {
-        factory(Product::class)->create();
+        $product = factory(Product::class)->create();
+    } else {
+        $product = Product::inRandomOrder()->first();
     }
+    
+    $percents = array(0.6, 0.7, 0.8, 0.9);
+    $randomPercent = $faker->randomElement($percents);
+
+    // set purchase price
+    $purchasePrice = round($product->base_price * $randomPercent, 2);
+
+    //set sale price
+    $salePrice = round($purchasePrice * 1.2, 2);
+
+    // set quantity
+    $quantityArray = array(5,0,55,110,260,340,580,727,1450);
+
+    // quantity array key
+    $qKey = array_rand($quantityArray);
+
+
     if (!User::first()) {
         factory(User::class)->create();
     }
+
     return [
-        'serial' => rand(100, 500000),
+        'serial' => rand(100000, 500000),
         'quantity' => 2000,
         'purchase_price' => $purchasePrice,
         'sale_price' => $salePrice,
         'quantity' => $quantityArray[$qKey],
         'user_id' => User::latest()->first()->id,
-        'product_id' => Product::latest()->first()->id,
+        'product_id' => $product->id,
         'expiration_at' => Carbon::now()->addDays(rand(50,365))->toDateTimeString()
     ];
 });
