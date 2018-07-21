08f6766228c96f38ba5575ab1c67f98c59dd72ff
yalin <yalincobanoglu@yandex.com>
2018-07-21 12:56:41 +0300

    base price

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,6 +6,7 @@ use App\Product;
 
 $factory->define(Product::class, function(Faker $faker){
     return [
-        'name' => $faker->sentence($nbWords = 2, $variableNbWords = false) . " " . $faker->randomElement($array = array ('50CC', '75CC', '100CC', '25GR', '50GR', '100GR'))
+        'name' => $faker->sentence($nbWords = 2, $variableNbWords = false) . " " . $faker->randomElement($array = array ('50CC', '75CC', '100CC', '25GR', '50GR', '100GR')),
+        'base_price' => round($faker->randomFloat($nbMaxDecimals = 4, $min = 5, $max = 20), 2),
     ];
 });
