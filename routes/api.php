c2c5f1d0a280589b10c604ffe7f2d48bb454dedc
yalin <yalincobanoglu@yandex.com>
2018-07-25 12:41:23 +0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ Route::get('/user', function(Request $request){
     return response()->json('HELLO');
 });
 
-Route::get('/customer/search/{customerName}', 'Api\ApiController@customerSearch')->name('api.customer.search');
+Route::get('/customer/search/{customerName}', 'Api\CustomerApiController@customerSearch')->name('api.customer.search');
 
-Route::get('/customer/detail/{customer}', 'Api\ApiController@customerDetail')->name('api.customer.detail');
+Route::get('/customer/detail/{customer}', 'Api\CustomerApiController@customerDetail')->name('api.customer.detail');
+
+// Route::get('/product/search/{productName}', 'Api\ApiController@productSearch')->name('api.product.search');
