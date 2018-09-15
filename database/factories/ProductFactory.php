c99a0b573468607aff3d229b09b413f2ca7944bf
yalin <yalincobanoglu@yandex.com>
2018-09-15 22:28:17 +0300

    fact

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,10 +3,9 @@
 use Faker\Generator as Faker;
 use App\Product;
 
-
-$factory->define(Product::class, function(Faker $faker){
+$factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->sentence($nbWords = 2, $variableNbWords = false) . " " . $faker->randomElement($array = array ('50CC', '75CC', '100CC', '25GR', '50GR', '100GR')),
-        'base_price' => round($faker->randomFloat($nbMaxDecimals = 4, $min = 5, $max = 20), 2),
+        'name' => $faker->sentence($nbWords = 2, $variableNbWords = false) . " " . $faker->randomElement($array = array('50CC', '75CC', '100CC', '25GR', '50GR', '100GR')),
+        'base_price' => random_int(300, 9000),
     ];
 });
