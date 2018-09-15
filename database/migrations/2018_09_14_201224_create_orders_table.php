a7ddffde1897e9f1bd66bbf0648d58253b13721a
yalin <yalincobanoglu@yandex.com>
2018-09-15 11:42:34 +0300

    create orders migration

---
+++ database/migrations/2018_09_14_201224_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('user_id');
+            $table->enum('status', ["OPEN","WAITING","APPROVED","SHIPPED","DONE"])->default('OPEN');
+            $table->unsignedDecimal('sub_total', 8, 2)->default(0);
+            $table->unsignedDecimal('tax', 8, 2)->default(0);
+            $table->unsignedDecimal('total', 8, 2)->default(0);
+            $table->text('shipment_info')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
