b8efffcd54137a5cbf0ba174951023005fc5dd11
yalin <yalincobanoglu@yandex.com>
2018-09-15 12:09:49 +0300

    orders table enum from model

--- database/migrations/2018_09_14_201224_create_orders_table.php
+++ database/migrations/2018_09_14_201224_create_orders_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Order;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -17,7 +18,7 @@ class CreateOrdersTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('customer_id');
             $table->unsignedInteger('user_id');
-            $table->enum('status', ["OPEN","WAITING","APPROVED","SHIPPED","DONE"])->default('OPEN');
+            $table->enum('status', Order::STATUS)->default('OPEN');
             $table->unsignedDecimal('sub_total', 8, 2)->default(0);
             $table->unsignedDecimal('tax', 8, 2)->default(0);
             $table->unsignedDecimal('total', 8, 2)->default(0);
