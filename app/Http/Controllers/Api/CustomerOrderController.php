d7fc425a92225dc98772d053efdf24ad1f1c2d86
yalin <yalincobanoglu@yandex.com>
2018-09-15 11:43:19 +0300

    customer order controller

---
+++ app/Http/Controllers/Api/CustomerOrderController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Customer;
+use App\Http\Resources\Customer as CustomerResource;
+use Illuminate\Support\Facades\Validator;
+use App\Order;
+
+class CustomerOrderController extends Controller
+{
+    /**
+     * Customer Detail with ID
+     *
+     * @param Request $request
+     * @param Customer $customer
+     * @return void
+     */
+    public function create(Request $request, Customer $customer)
+    {
+        if (!$customer) {
+            return response()->json('Not Found')->setStatusCode(404);
+        }
+
+        $order = new Order(['status' => 'OPEN']);
+
+        return $customer->addOrder($order, $request->user());
+    }
+}
