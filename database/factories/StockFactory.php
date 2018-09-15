e353c72218058fc52243b75c7c7a44990b87031e
yalin <yalincobanoglu@yandex.com>
2018-09-15 19:13:22 +0300

    stock factory updated

--- database/factories/StockFactory.php
+++ database/factories/StockFactory.php
@@ -23,7 +23,7 @@ $factory->define(Stock::class, function (Faker $faker) {
     $salePrice = round($purchasePrice * 1.2, 2);
 
     // set quantity
-    $quantityArray = array(5,0,55,110,260,340,580,727,1450);
+    $quantityArray = array(55,110,260,340,580,727,1450);
 
     // quantity array key
     $qKey = array_rand($quantityArray);
@@ -35,12 +35,11 @@ $factory->define(Stock::class, function (Faker $faker) {
 
     return [
         'serial' => rand(100000, 500000),
-        'quantity' => 2000,
         'purchase_price' => $purchasePrice,
         'sale_price' => $salePrice,
         'quantity' => $quantityArray[$qKey],
         'user_id' => User::latest()->first()->id,
         'product_id' => $product->id,
-        'expiration_at' => Carbon::now()->addDays(rand(50,365))->toDateTimeString()
+        'expiration_at' => Carbon::now()->addDays(rand(50, 365))->toDateTimeString()
     ];
 });
