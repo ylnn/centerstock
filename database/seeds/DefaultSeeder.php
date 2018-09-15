afa91b5721fb900bf485b772c6d826b584d5a0b5
yalin <yalincobanoglu@yandex.com>
2018-09-15 22:28:41 +0300

    seeder

--- database/seeds/DefaultSeeder.php
+++ database/seeds/DefaultSeeder.php
@@ -14,6 +14,6 @@ class DefaultSeeder extends Seeder
         factory('App\Customer', 20)->create();
         factory('App\Product', 20)->create();
         factory('App\Stock', 200)->create();
-        factory('App\Area', 10)->create();
+        factory('App\Area', 3)->create();
     }
 }
