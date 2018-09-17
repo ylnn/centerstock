92fd11a09115b9ed305dbab67c635c38bb3f1710
yalin <yalincobanoglu@yandex.com>
2018-09-17 18:43:53 +0300

    order items table

---
+++ database/migrations/2018_09_15_204053_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('stock_id');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
