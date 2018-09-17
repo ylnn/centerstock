d66aecf288b3f410f4903a2ba08ffbd2a590b5f8
yalin <yalincobanoglu@yandex.com>
2018-09-17 18:44:05 +0300

    routes update

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
-
-Route::get('/user', function (Request $request) {
-    return response()->json('HELLO');
-});
-
 Route::middleware(['auth:api'])->group(function () {
     // Customer
     Route::get('/customer/search/{customerName}', 'Api\CustomerSearchController@index')->name('api.customer.search');
     Route::get('/customer/detail/{customer}', 'Api\CustomerDetailController@index')->name('api.customer.detail');
 
     // Product
-    Route::get('/product/search/{productName}', 'Api\ProductApiController@productSearch')->name('api.product.search');
-    Route::get('/product/detail/{product}', 'Api\ProductApiController@productDetail')->name('api.product.detail');
+    Route::get('/product/search/{productName}', 'Api\ProductController@productSearch')->name('api.product.search');
+    Route::get('/product/detail/{product}', 'Api\ProductController@productDetail')->name('api.product.detail');
     
     // Order
-    Route::get('/orderCreate/{customer}', 'Api\OrderByUserController@create')->name('api.order.create');
+    Route::post('/order/query', 'Api\OrderController@index')->name('api.customer.orders');
+    Route::get('/order/create/{customer}', 'Api\OrderController@create')->name('api.order.create');
 
-    // Order Index by Customer
-    Route::post('/orderQuery', 'Api\OrderQueryController@index')->name('api.customer.orders');
-
+    Route::post('/orderitem/create', 'Api\OrderItemController@create')->name('api.orderitem.create');
 
     // Route::post('/customer-orders', 'Api\CustomerOrderController@index')->name('api.customer.orders');
 
